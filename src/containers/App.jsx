505cdfe61c8d69ebd116f059b89680b0422a181f
topeas <peiqxin_cd@keruyun.com>
2017-06-12 18:13:38 +0800

    :construction: router jump with redux

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,38 +1,40 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import React, {Component} from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
 import logo from '../asset/imgs/logo.svg'
-import { getIndexData } from '../stores/reducer/reduceApp'
+import {getIndexData} from '../stores/reducer/reduceApp'
 import '../style/pages/App.less'
 import Hello from '../components/Hello'
+import {push} from 'connected-react-router'
 
 const mapStateToProps = state => ({
   test: state.test.toJS(),
 })
 
 const mapDispatchToProps = (dispatch) => {
-  const actions = bindActionCreators({getIndexData}, dispatch)
-  return {...actions, dispatch}
+  const jumpByredux = () => dispatch(push('/about'))
+  const actions = bindActionCreators({ getIndexData, jumpByredux }, dispatch)
+  return { ...actions, dispatch }
 }
 
 class App extends Component {
-  handleClick () {
+  handleClick() {
     return this.props.getIndexData()
   }
 
-  render () {
+  render() {
     return (
       <div className="App">
         <div className="App-header">
-          <p>点击logo</p>
-          <img src={logo} className="App-logo" onClick={this.handleClick.bind(this)} alt="logo"/>
+          <p>点击第一个logo，发送一个异步的请求</p>
+          <img className="App-logo" src={logo} onClick={this.handleClick.bind(this)} alt="logo"/>
         </div>
         <h2>裴大哥爱你哟</h2>
-        <Hello cc="裴大哥爱你"/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <p>点击第二个logo，进行redux的跳转</p>
+          <img className="App-logo" src={logo} onClick={this.props.jumpByredux} alt="logo"/>
+        </div>
       </div>
     )
   }
